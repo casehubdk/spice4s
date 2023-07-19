2cb21b88e1b2991d8f592d06d9c3684f5cd149cd
Valdemar Grange <randomvald0069@gmail.com>
2023-07-19 21:30:30 +0200

    more codec

--- client/src/main/scala/spice4s/client/models/ResourceType.scala
+++ client/src/main/scala/spice4s/client/models/ResourceType.scala
@@ -1,7 +1,18 @@
 package spice4s.client.models
 
-sealed abstract case class ResourceType private (value: String)
+import cats.data._
+import scalapb.validate._
+import spice4s.client.util._
+
+sealed abstract case class ResourceType private (value: String) extends Encodable[String] {
+  def encode = value
+}
 
 object ResourceType {
-  import com.authzed.api.v1.{permission_service => perm}
+  val validationRegex = "^([a-z][a-z0-9_]{1,61}[a-z0-9]/)?[a-z][a-z0-9_]{1,62}[a-z0-9]$".r
+
+  def unsafeFromString(value: String): ResourceType = new ResourceType(value) {}
+
+  def apply(value: String): Validated[List[ValidationFailure],ResourceType] = 
+    unsafeFromString(value).validateRegex(validationRegex)
 }
--- client/src/main/scala/spice4s/client/util/package.scala
+++ client/src/main/scala/spice4s/client/util/package.scala
@@ -3,14 +3,19 @@ package spice4s.client
 import scalapb.validate._
 import cats.data._
 import cats.implicits._
+import scala.util.matching.Regex
 
 package object util {
-  implicit class EncodableOps[A](private val value: A) {
+  implicit class AnyUtilOps[A](private val value: A) {
     def validate[V: Validator](implicit ev: A <:< Encodable[V]): Validated[List[ValidationFailure],A] =
       Validator[V].validate(value.encode) match {
         case Failure(violations) => violations.invalid
         case Success => value.valid
       }
+
+    def validateRegex(regex: Regex)(implicit ev: A <:< Encodable[String]): Validated[List[ValidationFailure],A] =
+      if (regex.matches(value.encode)) value.valid
+      else List(ValidationFailure("regex-field", value.encode, s"$value with encoded value ${value.encode} does not match regex ${regex.toString}")).invalid
   }
 }
 
