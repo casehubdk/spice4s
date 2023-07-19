64dd3ec28aae366188223a7e02c8c4e46c1fa6a3
Valdemar Grange <randomvald0069@gmail.com>
2023-07-19 21:41:33 +0200

    relation

---
+++ client/src/main/scala/spice4s/client/models/Relation.scala
@@ -0,0 +1,18 @@
+package spice4s.client.models
+
+import cats.data._
+import scalapb.validate._
+import spice4s.client.util._
+
+sealed abstract case class Relation private (value: String) extends Encodable[String] {
+  def encode = value
+}
+
+object Relation {
+  val validationRegex = "^([a-z][a-z0-9_]{1,62}[a-z0-9])?$".r
+
+  def unsafeFromString(value: String): Relation = new Relation(value) {}
+
+  def apply(value: String): Validated[List[ValidationFailure],Relation] = 
+    unsafeFromString(value).validateRegex(validationRegex)
+}
