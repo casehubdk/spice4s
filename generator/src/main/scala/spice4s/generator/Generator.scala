d13c00125f8e25bb54499f063d9d5f645cff0e29
Valdemar Grange <randomvald0069@gmail.com>
2023-08-03 17:13:25 +0200

    ci

--- generator/src/main/scala/spice4s/generator/Generator.scala
+++ generator/src/main/scala/spice4s/generator/Generator.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 CaseHubDK
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package spice4s.generator
 
 import cats.effect.{Resource => _, _}
@@ -283,7 +299,7 @@ object Generator extends App {
       )
       val all = prefix ++ xs
       all.map(_.syntax).mkString("\n\n")
-      // q"..$all".syntax
+    // q"..$all".syntax
     }
 
   def generateFromTo[F[_]: Files](from: Path, to: Path)(implicit F: Async[F]): F[Option[NonEmptyChain[String]]] =
