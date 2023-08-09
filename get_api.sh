f0eb3d657cdcca4d96085b198557c302201bc6e9
ValdemarGr <randomvald0069@gmail.com>
2023-08-09 14:57:26 +0200

    api script

--- get_api.sh
+++ get_api.sh
@@ -7,3 +7,27 @@ mv ./proto/src/main/protobuf/api-main/authzed/api/* ./proto/src/main/protobuf/au
 rm -rf ./proto/src/main/protobuf/authzed/api/v1/openapi.proto
 rm -rf ./proto/src/main/protobuf/api.tar.gz
 rm -rf ./proto/src/main/protobuf/api-main
+
+cat << EOF > ./proto/src/main/protobuf/authzed/api/v1/google_api_package.proto
+syntax = "proto3";
+import "scalapb/scalapb.proto";
+
+package google.api;
+
+option (scalapb.options) = {
+  scope: PACKAGE
+  flat_package: false
+};
+EOF
+
+cat << EOF > ./proto/src/main/protobuf/authzed/api/v1/authzed_api_package.proto
+syntax = "proto3";
+import "scalapb/scalapb.proto";
+
+package authzed.api;
+
+option (scalapb.options) = {
+  scope: PACKAGE
+  flat_package: false
+};
+EOF
